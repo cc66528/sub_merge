344b08ab04c65acd19a933ce468646097fc3c61e
alanbobs999 <alan999@foxmail.com>
2021-10-06 17:19:04 +0800

    更新列表

--- list_merge.py
+++ list_merge.py
@@ -45,20 +45,21 @@ class sub_convert():# 将订阅链接中YAML，Base64等内容转换为 Url 链�
         s.mount('http://', HTTPAdapter(max_retries=3))
         s.mount('https://', HTTPAdapter(max_retries=3))
         try:
-            print('Downloading from:' + self.sub_url + '\n')
+            print('Downloading from:' + self.sub_url)
             resp = s.get(self.sub_url, timeout=5)
-            sub_content = resp.content.decode('utf-8')
+            sub_content = resp.content.decode('utf-8') 
 
             if 'proxies:' in sub_content: # 判断字符串是否在文本中，是，判断为YAML。https://cloud.tencent.com/developer/article/1699719
                 self.url_content = sub_convert.yaml_decode(sub_content)
-                return self.url_content
+                return ''
+                #return self.url_content.replace('\r','') # 去除‘回车\r符’ https://blog.csdn.net/jerrygaoling/article/details/81051447
             elif '://'  in sub_content: # 同上，是，判断为 Url 链接内容。
                 self.url_content = sub_content
-                return self.url_content
+                return self.url_content.replace('\r','')
             else: # 判断 Base64.
                 self.url_content = sub_convert.base64_decode(sub_content)
                 self.url_content = base64.b64decode(sub_content.encode('utf-8')).decode('ascii')
-                return self.url_content
+                return self.url_content.replace('\r','')
 
         except requests.exceptions.RequestException as err:
             print(err)
@@ -91,15 +92,21 @@ class sub_merge(): # 将转换后的所有 Url 链接内容合并转换 YAML or 
         content_list = []
         for index in range(len(self.url_list)):
             content_list.append(sub_convert(self.url_list[index],'').url_encode())
-            file = open('./sub/list/' + str(index) + '.txt', 'w', encoding = 'utf-8')
-            file.write(content_list[index])
-            file.close
+            try:
+                file = open('./sub/list/' + str(index) + '.txt', 'w', encoding = 'utf-8')
+                file.write(content_list[index])
+                print('Writing ' + raw_list[index]['remarks'] + ' to ' + str(index) + '.txt')
+            except TypeError:
+                print('Url 订阅内容为空')
+            finally:
+                file.close()
         
         print('Merging nodes...')
         content = ''.join(content_list) # https://python3-cookbook.readthedocs.io/zh_CN/latest/c02/p14_combine_and_concatenate_strings.html
 
         content_base64 = sub_convert.base64_encode(content)
         content_yaml = sub_convert.yaml_decode(content)
+        print('Done!')
                    
         file = open('./sub/sub_merge.txt', 'w', encoding = 'utf-8')
         file.write(content)
