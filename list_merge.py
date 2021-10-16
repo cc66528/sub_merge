e6d52af3c777c684e579a43f6d88ad976bd0c409
alanbobs999 <alan999@foxmail.com>
2021-10-16 09:52:58 +0800

    Update utils

--- list_merge.py
+++ list_merge.py
@@ -1,80 +1,23 @@
 #!/usr/bin/env python3
 
-from pathlib import Path
+from sub_convert import sub_convert # Python 之间互相调用文件https://blog.csdn.net/winycg/article/details/78512300
+from list_update import update_url
+
 import json
-import yaml
-import base64
-from urllib.parse import quote
-import requests
-from requests.adapters import HTTPAdapter
+import os
+import re
+
 
 # 分析当前项目依赖 https://blog.csdn.net/lovedingd/article/details/102522094
 
 
 # 文件路径定义
+
+#Eterniy = './Eternity'
+
 sub_list_json = './sub/sub_list.json'
-sub_merge_path = './sub'
+sub_merge_path = './sub/'
 sub_list_path = './sub/list/'
-
-
-class sub_convert():# 将订阅链接中YAML，Base64等内容转换为 Url 链接内容
-    
-    def __init__(self,sub_url,output_type):
-        self.sub_url = sub_url
-        self.output_type = output_type
-        self.url_content = ''
-    
-    def yaml_decode(content): # YAML 转换为 Url 链接内容
-        yaml_content = yaml.dump(content)
-        return yaml_content
-    def base64_decode(content): # Base64 转换为 Url 链接内容
-        base64_content = base64.b64decode(content.encode('utf-8')).decode('ascii')
-        return base64_content
-
-    def url_encode(self):# 读取订阅内容，并转化为 Url 链接内容
-
-        s = requests.Session()
-        s.mount('http://', HTTPAdapter(max_retries=3))
-        s.mount('https://', HTTPAdapter(max_retries=3))
-        try:
-            print('Downloading from:' + self.sub_url)
-            resp = s.get(self.sub_url, timeout=5)
-            sub_content = resp.content.decode('utf-8') 
-
-            if 'proxies:' in sub_content: # 判断字符串是否在文本中，是，判断为YAML。https://cloud.tencent.com/developer/article/1699719
-                self.url_content = sub_convert.yaml_decode(sub_content)
-                return self.url_content.replace('\r','')
-                #return self.url_content.replace('\r','') # 去除‘回车\r符’ https://blog.csdn.net/jerrygaoling/article/details/81051447
-            elif '://'  in sub_content: # 同上，是，判断为 Url 链接内容。
-                self.url_content = sub_content
-                return self.url_content.replace('\r','')
-            else: # 判断 Base64.
-                try:
-                    self.url_content = sub_convert.base64_decode(sub_content)
-                    self.url_content = base64.b64decode(sub_content.encode('utf-8')).decode('ascii')
-                    return self.url_content.replace('\r','')
-                except Exception: # 万能异常 https://blog.csdn.net/Candance_star/article/details/94135515
-                    print('Url 订阅内容无法解析')
-                    return 'Url 订阅内容无法解析'
-
-        except Exception as err:
-            print(err)
-            return 'Url 解析错误'
-
-        
-    def yaml_encode(url_content): # 将 Url 内容转换为 YAML 
-        yaml_content = url_content
-        return yaml_content
-    def base64_encode(url_content): # 将 Url 内容转换为 Base64
-        base64_content = base64.b64encode(url_content.encode('utf-8')).decode('ascii')
-        return base64_content
-
-    def convert(self): # convert Url to YAML or Base64
-
-        if self.output_type == 'YAML':
-            return sub_convert.yaml_encode(self.url_content)
-        elif self.output_type == 'Base64':
-            return sub_convert.base64_encode(self.url_content)
 
 
 class sub_merge(): # 将转换后的所有 Url 链接内容合并转换 YAML or Base64, ，并输出文件，输入订阅列表。
@@ -87,51 +30,74 @@ class sub_merge(): # 将转换后的所有 Url 链接内容合并转换 YAML or 
 
         content_list = []
         for index in range(len(self.url_list)):
-            content = sub_convert(self.url_list[index],'').url_encode()
-            ids = int(sub_list[index]['id'])
-            remarks = sub_list[index]['remarks']
+            content = sub_convert.convert(self.url_list[index]['url'],'url','url')
+            ids = self.url_list[index]['id']
+            remarks = self.url_list[index]['remarks']
             #try:
             if content == 'Url 解析错误':
-                file = open(f'{sub_list_path}{ids:0>2d}.txt', 'w', encoding = 'utf-8')
+                file = open(f'{sub_list_path}{ids:0>2d}.txt', 'w', encoding= 'utf-8')
                 file.write('Url 解析错误')
                 file.close()
                 print(f'Writing error of {remarks} to {ids:0>2d}.txt\n')
             elif content == 'Url 订阅内容无法解析':
-                file = open(f'{sub_list_path}{ids:0>2d}.txt', 'w', encoding = 'utf-8')
+                file = open(f'{sub_list_path}{ids:0>2d}.txt', 'w', encoding= 'utf-8')
                 file.write('Url 订阅内容无法解析')
                 file.close()
                 print(f'Writing error of {remarks} to {ids:0>2d}.txt\n')
-            else:
+            elif content != None:
                 content_list.append(content)
-                file = open(f'{sub_list_path}{ids:0>2d}.txt', 'w', encoding = 'utf-8')
+                file = open(f'{sub_list_path}{ids:0>2d}.txt', 'w', encoding= 'utf-8')
                 file.write(content)
                 file.close()
                 print(f'Writing content of {remarks} to {ids:0>2d}.txt\n')
+            else:
+                file = open(f'{sub_list_path}{ids:0>2d}.txt', 'w', encoding= 'utf-8')
+                file.write('Url 订阅内容无法解析')
+                file.close()
+                print(f'Writing error of {remarks} to {ids:0>2d}.txt\n')
         
         print('Merging nodes...\n')
-        content = '\n'.join(content_list) # https://python3-cookbook.readthedocs.io/zh_CN/latest/c02/p14_combine_and_concatenate_strings.html
-        content_base64 = sub_convert.base64_encode(content)
-        content_yaml = sub_convert.yaml_encode(content)
+        content = sub_convert.rm_dup(''.join(content_list)) # https://python3-cookbook.readthedocs.io/zh_CN/latest/c02/p14_combine_and_concatenate_strings.html
+        content_base64 = sub_convert.convert(content,'content','Base64')
+        content_yaml = sub_convert.convert(content,'content','YAML')
 
         def content_write(file, output_type):
             file = open(file, 'w', encoding = 'utf-8')
             file.write(output_type)
             file.close
-        write_list = [f'{sub_merge_path}/sub_merge.txt', f'{sub_merge_path}/sub_merge_base64.txt', f'{sub_merge_path}/sub_merge_yaml.txt']
+        write_list = [f'{sub_merge_path}/sub_merge.txt', f'{sub_merge_path}/sub_merge_base64.txt', f'{sub_merge_path}/sub_merge_yaml.yml']
         content_type = (content, content_base64, content_yaml)
         for index in range(len(write_list)):
             content_write(write_list[index], content_type[index])
         print('Done!')
 
+def read_list():
+    with open(sub_list_json, 'r', encoding='utf-8') as f: # 将 sub_list.json Url 内容读取为列表
+        raw_list = json.load(f)
+    input_list = []
+    for index in range(len(raw_list)):
+        if raw_list[index]['enabled']:
+            urls = re.split('\|',raw_list[index]['url'])
+            if len(urls) > 1:
+                for url in urls:
+                    single_raw_list = raw_list[index]
+                    single_raw_list['url'] = url
+                    input_list.append(single_raw_list)
+            input_list.append(raw_list[index])
+    return input_list
 
-with open(sub_list_json, 'r', encoding='utf-8') as f:
-    raw_list = json.load(f)
-sub_list = []
-for index in range(len(raw_list)): # 将 sub_list.json Url 内容读取为列表
-    if raw_list[index]['enabled']:
-        sub_list.append(raw_list[index])
+""" def eternity_convert():
+    file_eternity = open(Eterniy, 'r', encoding='utf-8')
+    sub_content = file_eternity.read()
+    eternity_convert = sub_convert.convert(sub_content,'content','YAML')
+    file_eternity.close()
 
-input_list = []
-for index in range(len(sub_list)): # 将 sub_list.json Url 内容读取为列表
-        input_list.append(sub_list[index]['url'])
-run = sub_merge(input_list).merge()
+    eternity_yml = open('Eternity.yml', 'w', encoding= 'utf-8')
+    eternity_yml.write(eternity_convert)
+    eternity_yml.close()
+ """
+
+input_list = read_list()
+update = update_url.update([0,])
+merge = sub_merge(input_list).merge()
+#convert = eternity_convert()
--- list_update.py
+++ list_update.py
@@ -4,19 +4,27 @@ from datetime import timedelta, datetime
 import json
 
 # 文件路径定义
-sub_list_file = './sub/sub_list.json'
+sub_list_json = './sub/sub_list.json'
 
 
-with open(sub_list_file, 'r', encoding='utf-8') as f: # 载入订阅链接
+with open(sub_list_json, 'r', encoding='utf-8') as f: # 载入订阅链接
     raw_list = json.load(f)
     f.close()
 
 class update_url():
 
-    def update(id_list=[]):
-        for id in id_list:
-            if id == 0:
-                update_url.update_id_0()
+    def update(id_allow_list=[]):
+        if len(id_allow_list) > 0:
+            for id in id_allow_list:
+                if id == 0:
+                    update_url.update_id_0()
+
+            updated_list = json.dumps(raw_list, sort_keys=False, indent=2, ensure_ascii=False)
+            file = open(sub_list_json, 'w', encoding='utf-8')
+            file.write(updated_list)
+            file.close()
+        else:
+            print('Don\'t need to update.')
                 
     def update_id_0(): # remarks: pojiezhiyuanjun/freev2, 将原链接更新至 https://raw.fastgit.org/pojiezhiyuanjun/freev2/master/%MM%(DD - 1).txt
         raw_url = raw_list[0]['url']
@@ -24,13 +32,3 @@ class update_url():
         url_update = raw_url[:-8] + yesterday + raw_url[-4:]# 修改字符串中的某一位字符 https://www.zhihu.com/question/31800070/answer/53345749
         print(f'Change id 0 url to : {url_update}\n')
         raw_list[0]['url'] = url_update
-
-
-def update_allow_list(update_id_list=[]):
-    update_url.update(update_id_list)
-    updated_list = json.dumps(raw_list, sort_keys=False, indent=2, ensure_ascii=False)
-    file = open(sub_list_file, 'w', encoding='utf-8')
-    file.write(updated_list)
-    file.close()
-
-update_allow_list([0,])
